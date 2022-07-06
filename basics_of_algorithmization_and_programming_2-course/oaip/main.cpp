4898f1a26260a8dc0b9c5b4563719a316efdd42f
asp1rant3 <asp1rantzenevich@gmail.com>
2022-07-06 19:52:36 +0300

    #

---
+++ basics_of_algorithmization_and_programming_2-course/oaip/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class triangle
+{
+public:
+	triangle(int x, int o, int y)
+	{
+		cout << "Вывод значения x: " << x << endl;
+		cout << "Вывод значения o: " << o << endl;
+		cout << "Вывод значения y: " << y << endl;
+	}
+	triangle(const triangle& obj)
+	{
+		cout << "Конструктор копирования" << endl;
+	}
+	~triangle() {}
+
+};
+
+class point
+{
+public:
+	point(int x, int y)
+	{
+		
+	}
+};
+
+
+int main()
+{
+	int p;
+	cout << "Enter number: ";
+	cin >> p;
+	switch (p) {
+	case 1: {
+		cout << "xd" << endl;
+	}
+	case 2: {
+
+	}
+	default: {
+		cout << "Неверный номер" << endl;
+		break;
+	}
+	}
+	return 0;
+}
